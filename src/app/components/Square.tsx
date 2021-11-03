878fcbf5a87c5627d302119242402eeeb54ca34e
PReichetanz <paul.reichetanzgmx.de>
2021-11-03 09:23:11 +0100

    Finish Square after forgetting to save

--- src/app/components/Square.tsx
+++ src/app/components/Square.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
-function Square(props): JSX.Element {
+function Square(props: {
+  onClick: () => void;
+  value:
+    | boolean
+    | React.ReactChild
+    | React.ReactFragment
+    | React.ReactPortal
+    | null
+    | undefined;
+}): JSX.Element {
   return (
     <button className="square" onClick={() => props.onClick()}>
       {props.value}
