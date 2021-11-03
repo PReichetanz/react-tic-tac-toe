2b72469c5b0fe00958bec3bbaab31097a00ae049
PReichetanz <paul.reichetanzgmx.de>
2021-11-03 09:22:27 +0100

    Finish tic tac toe including time travel and types

--- src/app/components/Board.tsx
+++ src/app/components/Board.tsx
@@ -1,59 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Square from './Square';
 
-function Board(): JSX.Element {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-
-  function handleClick(i) {
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    const newSquares = squares.slice();
-    newSquares[i] = xIsNext ? 'X' : 'O';
-    setSquares(newSquares);
-    setXIsNext(!xIsNext);
-  }
-
+function Board(props: {
+  squares: any[];
+  onClick: (arg0: number) => unknown;
+}): JSX.Element {
   function renderSquare(i: number) {
-    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
-  }
-
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
-
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = 'Winner: ' + winner;
-  } else {
-    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    return <Square value={props.squares[i]} onClick={() => props.onClick(i)} />;
   }
 
   return (
     <div>
-      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
